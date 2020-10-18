b052fc0c5847230a5fc4539439d6924ae058d837
Rajat Bansal <rjtbansal@gmail.com>
2020-10-17 22:25:48 -0400

    Added prettier

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,27 @@
-import Head from 'next/head'
+import Head from "next/head";
 import axios from "axios";
-import MenuList from '../components/MenuList';
+import MenuList from "../components/MenuList";
 
 export async function getStaticProps() {
-  try{
-  const recipesDataReceived = await axios.get("https://fridge.kitchenmate.com/api/public/menus/95/recipes");
-  const recipesData = recipesDataReceived.data.map(({ name, category, summary, side_photo }) => ({ name, category, summary, side_photo }));
-  return {
-    props: {
-      recipesData: recipesData
-    }
-  }
+  try {
+    const recipesDataReceived = await axios.get(
+      "https://fridge.kitchenmate.com/api/public/menus/95/recipes"
+    );
+    const recipesData = recipesDataReceived.data.map(
+      ({
+        id,
+        name,
+        category,
+        summary,
+        side_photo,
+        nutrition: { allergens },
+      }) => ({ id, name, category, summary, side_photo, allergens })
+    );
+    return {
+      props: {
+        recipesData: recipesData,
+      },
+    };
   } catch (err) {
     console.error("Unexpected error ocurred while fetching recipes data", err);
   }
@@ -27,5 +38,5 @@ export default function Home({ recipesData }) {
         <MenuList recipesData={recipesData} />
       </main>
     </div>
-  )
+  );
 }
