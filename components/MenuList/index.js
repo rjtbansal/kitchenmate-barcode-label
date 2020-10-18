ffafd757f3382f0f950ef9d571d882809bc92cb1
Rajat Bansal <rjtbansal@gmail.com>
2020-10-18 14:41:57 -0400

    added intolerances and visible allergens

--- components/MenuList/index.js
+++ components/MenuList/index.js
@@ -3,16 +3,21 @@ import classes from "./menu-list.module.scss";
 export default function MenuList({ recipesData }) {
   return (
     <>
-      {recipesData.map(({id, name, category, summary, side_photo, allergens, barcodeURL }) => (
+      {recipesData.map(({id, name, category, summary, visible_allergens, visible_intolerances, barcodeURL }) => (
         <div key={id}>
           <h2>{name}</h2>
           <h3>{category}</h3>
           <p>{summary}</p>
           <p>
-            {" "}
-            Allergens:{" "}
-            {allergens.map((allergen, index) =>
-              index !== allergens.length - 1 ? `${allergen}, ` : allergen
+            Contains:
+            {visible_allergens.map((allergen, index) =>
+              index !== visible_allergens.length - 1 ? `${allergen}, ` : allergen
+            )}
+          </p>
+           <p>
+            Intolerances:
+            {visible_intolerances.map((intolerance, index) =>
+              index !== visible_intolerances.length - 1 ? `${intolerance}, ` : intolerance
             )}
           </p>
           <img src={barcodeURL} alt="" />
