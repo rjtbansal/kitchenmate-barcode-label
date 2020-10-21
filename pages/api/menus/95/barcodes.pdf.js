feb30e1f3a5b7d85246e23c6f0dd66988137a1e6
Rajat Bansal <rjtbansal@gmail.com>
2020-10-20 21:15:54 -0400

    added api /localhost:3000/api/menus/95/barcodes.pdf and getting the pdf URL

---
+++ pages/api/menus/95/barcodes.pdf.js
@@ -0,0 +1,26 @@
+/**APi Endpoint: http://localhost:3000/api/menus/95/barcodes.pdf */
+import axios from "axios";
+
+/**Current tunnel address: https://8b17b019b47f.ngrok.io/ */
+
+export default async function handler(req, res) {
+
+  try {
+    const receivePDFForRecipes = await axios.post("https://dev-pdfkm.now.sh", {
+      type: "raw",
+      urls: ["https://8b17b019b47f.ngrok.io/api/menus/95/barcodes"]
+    },
+      {
+        headers: {
+          token: "TFgOm0rIaXSwFdiyy2pcfshWLQ0qXQR6",
+        }
+      }
+    );
+    return res.status(200).json({ pdfUrl: receivePDFForRecipes.data[0] });
+
+  }
+  catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: "Unknown error occurred while fetching PDF" });
+  }
+}
